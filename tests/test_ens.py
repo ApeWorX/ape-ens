fe647f1ca3856a3dfa2a695753cf30d9bcd8687e
unparalleled-js <yingthi@live.com>
2022-09-26 14:50:03 -0500

    chore: rm unneeded gitignore change

--- tests/test_ens.py
+++ tests/test_ens.py
@@ -1,25 +1,42 @@
+import ape
 import pytest
-from ape.types import AddressType
+
+from tests.conftest import negative_tests
+
+
+@pytest.fixture
+def connection_spy(mocker, converter):
+    return mocker.spy(converter.network_manager, "get_provider_from_choice")
 
 
 def test_is_convertible(converter):
     assert converter.is_convertible("test.eth")
 
 
-@pytest.mark.parametrize(
-    "value",
-    (
-        "test",
-        "0xe1122aa5533228143C4Ce8fC4642aa33b857B332",
-        23452345,
-        "0x07D75c30f0217c99BD0bbeA00806E9d5D7E8EFA33b5852694A5bAf3D8141d432",
-    ),
-)
+@negative_tests
 def test_is_not_convertible(converter, value):
     assert not converter.is_convertible(value)
 
 
-def test_address_cache(converter):
-    new_address: AddressType = "0xe2222bb6633228143C4Ce8fC4642aa33b857B332"  # type: ignore
-    converter.address_cache["test.eth"] = new_address
-    assert converter.convert("test.eth") == new_address
+def test_address_cache(converter, address):
+    converter.address_cache["test.eth"] = address
+    assert converter.convert("test.eth") == address
+
+
+def test_mainnet_fork(converter, connection_spy):
+    with ape.networks.parse_network_choice("ethereum:mainnet-fork:mock-mainnet-fork"):
+        connection_spy.reset_mock()
+        converter.convert("test.eth")
+
+    # Should not have to reconnect
+    # TODO: Uncomment once #13 is fixed
+    # assert not connection_spy.call_count
+
+
+def test_other_ecosystem_mainnet(converter, connection_spy):
+    with ape.networks.parse_network_choice("polygon:mainnet:mock-polygon-mainnet"):
+        connection_spy.reset_mock()
+        converter.convert("test.eth")
+
+    # It has to re-connect to Ethereum mainnet temporarily
+    connection_spy.assert_called_once_with("ethereum:mainnet")
