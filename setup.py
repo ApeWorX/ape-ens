57221b864236031d3c4fae6caa52b15908dc6ce7
slush <sam@apeworx.io>
2024-12-04 14:36:27 -0600

    chore: update setuptools for pep-625 (#39)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ extras_require = {
         "towncrier>=19.2.0, <20",  # Generate release notes
     ],
     "release": [  # `release` GitHub Action job uses this
-        "setuptools",  # Installation tool
+        "setuptools>=75.6.0",  # Installation tool
         "setuptools-scm",  # Installation tool
         "wheel",  # Packaging tool
         "twine",  # Package upload tool
