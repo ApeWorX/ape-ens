e834d6a1b65904473a538f3f20cd25afbe982ec5
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2024-12-04 16:19:43 -0500

    feat: upgrade web3

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "eth-ape>=0.8.1,<0.9",
-        "web3",  # Use same version as eth-ape
+        "web3>=6.20.3,<8",
     ],
     python_requires=">=3.9,<4",
     extras_require=extras_require,
