ApeWorX/ape-base

refactor: modernize python tooling

ape_base/__init__.py
@@ -54,18 +54,17 @@ def __getattr__(name: str):
 
         return NETWORKS
 
-    elif name == "Base":
+    if name == "Base":
         from .ecosystem import Base
 
         return Base
 
-    elif name == "BaseConfig":
+    if name == "BaseConfig":
         from .ecosystem import BaseConfig
 
         return BaseConfig
 
-    else:
-        raise AttributeError(name)
+    raise AttributeError(name)
 
 
 __all__ = [

ApeWorX/ape-ens

ape_ens/__init__.py
@@ -23,12 +23,12 @@ def __getattr__(name: str):
 
         return ENS
 
-    elif name == "ENSConfig":
+    if name == "ENSConfig":
         from ape_ens.config import ENSConfig
 
         return ENSConfig
 
-    elif name == "ENSConversions":
+    if name == "ENSConversions":
         from ape_ens.converter import ENSConversions
 
         return ENSConversions